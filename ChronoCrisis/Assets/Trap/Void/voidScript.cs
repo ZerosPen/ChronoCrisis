9a8048c41c161022c3154c9b0425998bdb27f8a1
astranot09 <anthony.setiawan002@binus.ac.id>
2025-02-18 12:31:02 +0700

    Dialog 3,5/8

---
+++ ChronoCrisis/Assets/Trap/Void/voidScript.cs
@@ -0,0 +1,24 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class voidScript : MonoBehaviour
+// {
+//     [SerializeField] private float damage;
+
+//     private void OnTrigger{
+//         if(Collision.tag == "player")
+//         Collision.GetComponent<health>;
+//     }
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+        
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+// }
