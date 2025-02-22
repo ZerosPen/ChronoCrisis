576d784b42b34834978808babb77594656ec8e5b
astranot09 <anthony.setiawan002@binus.ac.id>
2025-02-22 17:18:36 +0700

    tes

--- ChronoCrisis/Assets/ButtonManagement.cs
+++ ChronoCrisis/Assets/ButtonManagement.cs
@@ -34,8 +34,28 @@ public class ButtonManagement : MonoBehaviour
 
     //Main page
     public GameObject Setting;
+    public GameObject About;
+    public GameObject Exit;
     public void ClickToOpenSetting(){
         Setting.SetActive(true);
+    }
+    public void ClickToCloseSetting(){
+        Setting.SetActive(false);
+    }
+    public void ClickToOpenAbout(){
+        About.SetActive(true);
+    }
+    public void ClickToCloseAbout(){
+        About.SetActive(false);
+    }
+    public void ClickToOpenExit(){
+        Exit.SetActive(true);
+    }
+    public void ClickToCloseExit(){
+        Exit.SetActive(false);
+    }
+        public void ClickToExit(){
+        Application.Quit();
     }
 
 
